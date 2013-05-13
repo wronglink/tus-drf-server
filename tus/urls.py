991246a4bcfca8ea491df52f1b3448de9bc017fa
Michael Elovskikh <wronglink@gmail.com>
2013-05-13 23:15:50 +0600

    Partly implemented protocol support

--- tus/urls.py
+++ tus/urls.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
-from tus.views import FileUploadView, home
+from tus.views import home, files_root, file_upload
 
 urlpatterns = patterns('',
     url(r'^$', home,  name='home'),
-    url(r'^files/(?P<filename>[a-zA-Z0-9]+)/$', FileUploadView.as_view(), name='file-upload'),
+    url(r'^files/$', files_root, name='files-root'),
+    url(r'^files/(?P<filename>[a-zA-Z0-9]+)/$', file_upload, name='file-upload'),
 )
--- tus/views.py
+++ tus/views.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
-from rest_framework import views
+from django.utils.crypto import get_random_string
+from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 
 def home(request):
     return render_to_response('base.html')
 
 
-class FileUploadView(views.APIView):
-    parser_classes = (FileUploadParser,)
+@api_view(['POST'])
+def files_root(request, format=None):
+    filename = get_random_string(32)
+    headers = {
+        'Location': reverse('file-upload', kwargs={'filename': filename})
+    }
+    return Response(status=204, headers=headers)
 
-    def patch(self, request, filename, format=None):
-        file_obj = request.FILES['file']
-        return Response(status=204)
+
+@api_view(['PATCH'])
+@parser_classes([FileUploadParser])
+def file_upload(request, filename, format=None):
+    file_obj = request.FILES['file']
+    return Response(status=204)
