4c609987b67ba8cd260fd36e56bf0e97ab4f6a67
Michael Elovskikh <wronglink@gmail.com>
2013-05-14 19:54:02 +0600

    Some minor fixes

--- tus/urls.py
+++ tus/urls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
-from tus.views import home, files_root, file_upload
+from tus.views import home, files_root, FileUploadView
 
 urlpatterns = patterns('',
     url(r'^$', home,  name='home'),
     url(r'^files/$', files_root, name='files-root'),
-    url(r'^files/(?P<filename>[a-zA-Z0-9]+)/$', file_upload, name='file-upload'),
+    url(r'^files/(?P<filename>[a-zA-Z0-9]+)/$', FileUploadView.as_view(), name='file-upload'),
 )
--- tus/views.py
+++ tus/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework.views import APIView
 
 
 def home(request):
@@ -17,11 +18,17 @@ def files_root(request, format=None):
     headers = {
         'Location': reverse('file-upload', kwargs={'filename': filename})
     }
-    return Response(status=204, headers=headers)
+    return Response(status=201, headers=headers)
 
 
-@api_view(['PATCH'])
-@parser_classes([FileUploadParser])
-def file_upload(request, filename, format=None):
-    file_obj = request.FILES['file']
-    return Response(status=204)
+class FileUploadView(APIView):
+    parser_classes = [FileUploadParser]
+
+    def head(self, request, filename, format=None):
+        # TODO: Implement right offset header
+        return Response(status=204, headers={'Offset': 0})
+
+    def patch(self, request, filename, format=None):
+        file_obj = request.FILES['file']
+        # TODO: Implement uploaded chunk handing
+        return Response(status=204)
